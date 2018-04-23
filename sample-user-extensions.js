979ad1efd61b9f9ddb94a89dc5f529bdae724211
devalex88 <36872529+devalex88@users.noreply.github.com>
2018-04-23 14:15:31 +0700

    Add files via upload

--- sample-user-extensions.js
+++ sample-user-extensions.js
@@ -19,3 +19,17 @@ Selenium.prototype.doTypeRepeated = function(locator, text) {
     // Replace the element text with the new text
     this.page().replaceText(element, valueToType);
 };
+
+Selenium.prototype.assertValueRepeated = function(locator, text) {
+    // All locator-strategies are automatically handled by "findElement"
+    var element = this.page().findElement(locator);
+
+    // Create the text to verify
+    var expectedValue = text + text;
+
+    // Get the actual element value
+    var actualValue = element.value;
+
+    // Make sure the actual value matches the expected
+    Assert.matches(expectedValue, actualValue);
+};
